c9f53fc7b75ff62e824765958ae434d673b9627e
C0A21130 <c0a2113061@edu.teu.ac.jp>
2025-03-25 21:56:30 +0900

    Update Home component to fetch tokens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,39 @@
-import { useState } from 'react';
+import { useState, createContext, Dispatch, SetStateAction } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Wallet, HDNodeWallet } from 'ethers';
 import { AppShell, MantineProvider } from '@mantine/core';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import User from './pages/User';
 import './index.css';
 import '@mantine/core/styles.css';
 
+export const walletContext = createContext<[Wallet | HDNodeWallet | undefined, Dispatch<SetStateAction<Wallet | HDNodeWallet | undefined>>]>([undefined, () => {}]);
+
 function App() {
   const [opened, setOpened] = useState(false);
+  const [wallet, setWallet] = useState<Wallet | HDNodeWallet>();
 
   return (
     <BrowserRouter>
       <MantineProvider>
-        <AppShell>
-          <AppShell.Header>
-            <Header opened={opened} setOpened={setOpened} />
-          </AppShell.Header>
-          <AppShell.Navbar className='mt-8'>
-            <Navbar opened={opened} />
-          </AppShell.Navbar>
-          <AppShell.Main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </AppShell.Main>
-        </AppShell>
+        <walletContext.Provider value={[wallet, setWallet]}>
+          <AppShell>
+            <AppShell.Header>
+              <Header opened={opened} setOpened={setOpened} />
+            </AppShell.Header>
+            <AppShell.Navbar className='mt-8'>
+              <Navbar opened={opened} />
+            </AppShell.Navbar>
+            <AppShell.Main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/user" element={<User />} />
+              </Routes>
+            </AppShell.Main>
+          </AppShell>
+        </walletContext.Provider>
       </MantineProvider>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect, useContext } from 'react';
+import { Card, Image, Text, Group } from '@mantine/core';
+import { Container } from '@mantine/core';
+import { walletContext } from '../App';
+import fetchTokens from '../components/fetchTokens';
+
+const Home = () => {
+  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  const [wallet] = useContext(walletContext);
+  const [tokens, setTokens] = useState<Token[]>([]);
+
+  useEffect(() => {
+    const getTokens = async () => {
+      if (wallet == undefined) { return; }
+      const tokens = await fetchTokens(wallet, contractAddress);
+      setTokens(tokens);
+    }
+    getTokens();
+  }, []);
+
+  if(wallet == undefined) {
+    return (
+      <div className='mt-12'>
+        <Container className="mt-4">
+          <Card shadow="sm" padding="lg" className="mb-4">
+            <Text size="lg">Please Connect Wallet</Text>
+          </Card>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className='mt-12'>
+      <Container className="mt-4">
+        {tokens?.map((token, index) => (
+          <Card key={index} shadow="sm" padding="lg" className="mb-4">
+            <Card.Section>
+              <Image src={token} height={160} alt={token.name} />
+            </Card.Section>
+            <Group style={{ marginBottom: 5, marginTop: 10 }}>
+              <Text>{token.tokenId}</Text>
+            </Group>
+            <Text size="sm" style={{ lineHeight: 1.5 }}>
+              Address:
+              <br />
+              {token.owner}
+            </Text>
+          </Card>
+        ))}
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
