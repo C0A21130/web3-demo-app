fcf36cae65641b63499963a5a9f3c62895a83dd8
C0A21130 <c0a2113061@edu.teu.ac.jp>
2025-04-03 02:05:13 +0900

    Add RPC URL and contract constant address

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,35 +1,20 @@
 import { useState, useEffect, useContext } from 'react';
 import { Card, Text, Group } from '@mantine/core';
 import { Container } from '@mantine/core';
-import { walletContext } from '../App';
+import { contractAddress, walletContext } from '../App';
 import fetchTokens from '../components/fetchTokens';
 
 const Home = () => {
-  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   const [wallet] = useContext(walletContext);
   const [tokens, setTokens] = useState<Token[]>([]);
 
   useEffect(() => {
     const getTokens = async () => {
-      if (wallet == undefined) { return; }
       const tokens = await fetchTokens(wallet, contractAddress, "all");
       setTokens(tokens);
     }
     getTokens();
   }, []);
-
-  // If the wallet is not connected, display a message
-  if(wallet == undefined) {
-    return (
-      <div className='mt-12'>
-        <Container className="mt-4">
-          <Card shadow="sm" padding="lg" className="mb-4">
-            <Text size="lg">Please Connect Wallet</Text>
-          </Card>
-        </Container>
-      </div>
-    );
-  }
 
   // If there are no tokens, display a message
   if(tokens.length == 0) {
