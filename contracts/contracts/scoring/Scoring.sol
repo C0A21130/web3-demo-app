5ba32ffc219832e0ed9b9aec10cc8c4aca12dc83
YukiYasuno <85671824+C0A21130@users.noreply.github.com>
2025-10-03 02:00:26 +0900

    feat: integrate Scoring contract into SsdlabToken and enhance scoring functionality

--- contracts/contracts/scoring/Scoring.sol
+++ contracts/contracts/scoring/Scoring.sol
@@ -12,6 +12,7 @@ import "./IERC4974.sol";
 contract Scoring is Centrality, IERC4974 {
     mapping(address => bool) private operators;
     mapping(address => int8) private ratings;
+    mapping(address => int8) private scores;
 
     /// @dev See {IERC165-supportsInterface}.
     function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
@@ -29,6 +30,7 @@ contract Scoring is Centrality, IERC4974 {
     }
 
     /// @dev See {IERC4974-setOperator}.
+    /// 新しいTrust Score Agentのアドレスを設定するための関数
     function setOperator(address _operator) public onlyOperator {
         require(_operator != address(0), "Invalid operator address");
         require(!operators[_operator], "Operator already set");
@@ -37,20 +39,19 @@ contract Scoring is Centrality, IERC4974 {
     }
 
     /// @dev See {IERC4974-rate}.
+    /// Trust Score Agentが信用スコアを登録するための関数
     function rate(address _rated, int8 _rating) public onlyOperator {
+        // 既存のスコアを削除
         require(_rated != address(0), "Rated address cannot be zero");
         require(_rating >= -127 && _rating <= 127, "Rating must be between -127 and 127");
-        int8 score = 0;
-        if(_rating == -1){
-            score = Centrality.calculateDegreeCentrality(_rated);
-        } else {
-            score = _rating;
-        }
-        ratings[_rated] = score;
-        emit Rating(_rated, score);
+        
+        // 新しいスコアを登録
+        ratings[_rated] = _rating;
+        emit Rating(_rated, _rating);
     }
 
     /// @dev See {IERC4974-removeRating}.
+    /// Trust Score Agentが登録した信用スコアを削除するための関数
     function removeRating(address _removed) public onlyOperator {
         require(_removed != address(0), "Removed address cannot be zero");
         delete ratings[_removed];
@@ -58,14 +59,14 @@ contract Scoring is Centrality, IERC4974 {
     }
 
     /// @dev See {IERC4974-ratingOf}.
+    /// Trust Score Agentが登録した信用スコアを取得するための関数
     function ratingOf(address _rated) public view returns (int8) {
         return ratings[_rated];
     }
 
-    function registScore(address _user) public onlyOperator {
+    // 取引履歴から計算された信用スコアを取得するための関数
+    function getScore(address _user) public view returns (int8) {
         require(_user != address(0), "Invalid user address");
-
-        int8 score = Centrality.calculateDegreeCentrality(_user);
-        rate(_user, score);
+        return Centrality.calculateDegreeCentrality(_user);
     }
 }
--- contracts/test/scoring/Scoring.ts
+++ contracts/test/scoring/Scoring.ts
@@ -1,12 +1,12 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Scoring } from "../../typechain-types";
 
 async function deployFixture() {
   const [owner, addr1, addr2] = await ethers.getSigners();
-  const Scoring = await ethers.deployContract("Scoring", [addr1]);
-  return { Scoring, owner, addr1, addr2 };
+  const Scoring = await ethers.deployContract("Scoring", [owner.address]);
+  const SsdlabToken = await ethers.deployContract("SsdlabToken", [owner.address, addr1.address]);
+  return { Scoring, SsdlabToken, owner, addr1, addr2 };
 }
 
 describe("Scoring Contract", function () {
@@ -18,18 +18,41 @@ describe("Scoring Contract", function () {
     });
   });
 
-  describe("スコア管理", function () {
+  describe("Trust Score Agentによるスコア管理", function () {
     it("新しいオペレーターを追加できること", async function () {
-      const { Scoring, addr1, addr2 } = await loadFixture(deployFixture);
-      await Scoring.connect(addr1).setOperator(addr2.address);
+      const { Scoring, owner, addr1 } = await loadFixture(deployFixture);
+      await Scoring.connect(owner).setOperator(addr1.address);
     });
 
-    it("新しいスコアを追加・削除できること", async function () {
-      const { Scoring, addr1, addr2 } = await loadFixture(deployFixture);
-      await Scoring.connect(addr1).rate(addr2.address, 100);
-      expect(await Scoring.ratingOf(addr2.address)).to.equal(100);
-      await Scoring.connect(addr1).removeRating(addr2.address);
-      expect(await Scoring.ratingOf(addr2.address)).to.equal(0);
+    it("スコアを登録・削除・取得できること", async function () {
+      const { Scoring, owner, addr1 } = await loadFixture(deployFixture);
+      
+      // スコアの登録
+      await Scoring.connect(owner).rate(addr1.address, 85);
+      const score = await Scoring.ratingOf(addr1.address);
+      expect(score).to.equal(85);
+
+      // スコアの削除
+      await Scoring.connect(owner).removeRating(addr1.address);
+      const removedScore = await Scoring.ratingOf(addr1.address);
+      expect(removedScore).to.equal(0);
+    });
+  });
+
+  describe("スマートコントラクトによるスコアの算出", function () {
+    it("トークン発行に紐づけて取引履歴管理とスコア算出可能なこと", async function () {
+      const { SsdlabToken, addr1, addr2 } = await loadFixture(deployFixture);
+      
+      // トークンの発行と転送
+      let tx = await SsdlabToken.connect(addr1).safeMint(addr1.address, "tokenName1");
+      let receipt = await tx.wait();
+      const transferEvent = receipt?.logs.find(log => log.topics[0] === SsdlabToken.interface.getEvent("Transfer").topicHash);
+      const tokenId = transferEvent ? parseInt(transferEvent.topics[3], 16) : 0;
+      await SsdlabToken.connect(addr1).transferFrom(addr1.address, addr2.address, tokenId);
+
+      // スコアの取得
+      const score = await SsdlabToken.getScore(addr1.address);
+      expect(score).to.equal(100);
     });
   });
 });
