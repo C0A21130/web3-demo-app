c9e84d6f8884d6d30185271868e4692198908d62
YukiYasuno <85671824+C0A21130@users.noreply.github.com>
2026-03-24 17:06:07 +0900

    update: simplify hardhat configuration

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,14 +4,13 @@ import { configVariable, defineConfig } from "hardhat/config";
 export default defineConfig({
   plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-      production: {
-        version: "0.8.28",
-      },
-    }
+    },
   },
   networks: {
     hardhatMainnet: {
@@ -21,13 +20,6 @@ export default defineConfig({
     hardhatOp: {
       type: "edr-simulated",
       chainType: "op",
-    },
-    quorum: {
-      type: "http",
-      chainType: "l1",
-      url: "http://10.203.92.69:22000",
-      chainId: 1337,
-      accounts: ["0x9219477c394a4581aa7d440f85d77eee294580c17b5be14dbccea33d552687da"]
-    },
+    }
   }
 });
